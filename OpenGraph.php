4e2595ded987273dd8f8c0c39d90902c459ed4e3
PcFrk256 <PcFrk256@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-13 23:59:47 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-open-graph/trunk@396889 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ OpenGraph.php
@@ -0,0 +1,69 @@
+<?php
+/*
+    Plugin Name: Simple OpenGraph
+    Plugin URI: http://analteredreality.com/opengraph
+    Description: Plugin displays simple open graph data
+    Author: K. Bowers
+    Version: 0.1
+    Author URI: http://analteredreality.com
+    License: GPL2
+    */			
+/*  Copyright 2011  Kevin Bowers  (email : pcfrk256@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+function opengraph(){
+?>
+    <meta property="og:site_name" content="<?php echo get_bloginfo('name');?>"/>
+    <meta property="og:title" content="
+    <?php
+		global $post;
+		if (is_home()) $title = get_bloginfo('name');
+		elseif
+		(is_single()) $title = $post->post_title;
+		echo $title;
+	?>"/>
+    <meta property="og:type" content="
+    <?php
+		if ( is_home() ) $type = 'website';
+		elseif (empty($type)) $type = 'article';
+		echo $type;
+	?>"/>
+    <meta property="og:url" content="
+    <?php
+	if (is_home()) $url = home_url();
+	elseif (is_single()) $url= the_permalink();
+	echo $url
+	?>"/>
+    <meta property="og:image" content="
+    <?php
+    global $post;
+	if (is_singular() && empty($image) && has_post_thumbnail($post->ID)){
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail');
+	if ($thumbnail) {
+		$image = $thumbnail[0];
+	}}
+	echo $image;
+	?>"/>
+    <meta property="og:description" content="
+    <?php
+    $excerpt = get_the_excerpt();
+	if (is_single()) $description = strip_tags($excerpt);
+	elseif (is_home()) $description = get_bloginfo('description');
+	echo $description;
+	?>"/>
+<?php
+};
+add_action('wp_head', 'opengraph');
+?>
