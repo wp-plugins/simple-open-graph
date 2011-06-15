134b1cb6c0cb7c687ac44ccdc2e6cc8e15385508
PcFrk256 <PcFrk256@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-15 15:06:13 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/simple-open-graph/tags/0.3@397519 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/OpenGraph.php
@@ -0,0 +1,74 @@
+<?php
+/*
+    Plugin Name: Simple OpenGraph
+    Plugin URI: http://analteredreality.com/opengraph
+    Description: Plugin displays simple open graph data
+    Author: K. Bowers
+    Version: 0.2
+    Author URI: http://analteredreality.com
+    License: GPL2
+    */			
+	/*  Copyright 2011  Kevin Bowers  (email : pcfrk256@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*/
+function opengraph(){
+?>
+    <meta property="og:site_name" content="<?php echo get_bloginfo('name');?>"/>
+    <meta property="og:title" content="
+    <?php
+		global $post;
+		if (is_home()) $title = get_bloginfo('name');
+		elseif
+		(is_singular()) $title = the_title();
+		echo $title;
+	?>"/>
+    <meta property="og:type" content="
+    <?php
+		if ( is_home() ) $type = 'website';
+		elseif (empty($type)) $type = 'article';
+		echo $type;
+	?>"/>
+    <meta property="og:url" content="
+    <?php
+	if (is_home()) $url = home_url();
+	elseif (is_singular()) $url= the_permalink();
+	echo $url
+	?>"/>
+    <?php
+    global $post;
+	if (is_singular() && empty($image) && has_post_thumbnail($post->ID)){
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail');
+	if ($thumbnail) {
+		$image = $thumbnail[0];
+	}}
+	if (empty($image))
+	echo "";
+	else
+	echo  '<meta property="og:image" content="'.$image.'"/>';
+	?>
+    
+    <?php
+    $excerpt = get_the_excerpt();
+	if (is_single()) $description = strip_tags($excerpt);
+	elseif (is_home()) $description = get_bloginfo('description');
+	if (empty($description))
+	echo "";
+	else
+	echo '<meta property="og:description" content="'.$description.'"/>'
+	?>
+<?php
+};
+add_action('wp_head', 'opengraph');
+?>
---
+++ trunk/Readme.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+
+Contributors: pcfrk256
+Donate link: http://analteredreality.com/say-thanks
+Tags: facebook, open graph
+Requires at least: 2.0.2
+Tested up to: 3.1
+Stable tag: 0.3
+
+This simple plugin adds Facebook's Open Graph meta data to the header
+
+== Description ==
+
+Simple Open Graph adds Facebook's Open Graph meta data to the header. It's simple. Activate it and forget about it.
+
+== Installation ==
+
+1. Upload `simple_opengraph.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Relax
+
+== Frequently Asked Questions ==
+
+None
+
+== Changelog == 
+
+0.1 - Initial Release
+0.2 - Bug Fixes
+0.3 - Changed handling of empty image and Description
+
+== Upgrade Notice == 
+
+None yet.
+
+== Screenshots ==
+
+Look at the page source!
