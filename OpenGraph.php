a7cb5766e790b0741c10bce112fb1995fbbcd884
PcFrk256 <PcFrk256@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-15 18:54:26 +0000

    Updated Version number in OpenGraph.php
    git-svn-id: https://plugins.svn.wordpress.org/simple-open-graph/tags/2.3@1090641 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- OpenGraph.php
+++ OpenGraph.php
@@ -4,7 +4,7 @@
     Plugin URI: http://analteredreality.com/opengraph
     Description: Plugin displays simple open graph data
     Author: K. Bowers
-    Version: 2.2
+    Version: 2.3
     Author URI: http://analteredreality.com
     License: GPL2
 
